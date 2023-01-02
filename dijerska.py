9da1bcc82fe574285f7e34396dd46eb847b81048
Rouaa <lamoucha999@gmail.com>
2023-01-02 03:01:40 +0100

    msg

---
+++ dijerska.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def dijkstraAlgo(graph, vertex):
+    queue = deque([vertex])
+    distance = {vertex: 0}
+    while queue:
+        t = queue.popleft()
+        print("Visite du sommet " + str(t))
+        for voisin in graph[t]:
+                queue.append(voisin)
+                nouvelle_distance = distance[t] + graph[t][voisin]
+                if(voisin not in distance or nouvelle_distance < distance[voisin]):
+                    distance[voisin] = nouvelle_distance
+                    print("Met à jour le sommet " + str(voisin) + " avec la distance : " + str(nouvelle_distance))
+                    
+    return distance
+
+
+
+#Liste d'ajacence du graphe
+graph = {'A':{'B':135,'C':4},'B':{'E':5},'C':{'E':161,'D':2},'D':{'E':3},'E':{}}
+distance = dijkstraAlgo(graph,'A')
+print("Distances" + str(distance))
